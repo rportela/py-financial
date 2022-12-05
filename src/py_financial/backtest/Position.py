1eefa72e369e9e90fdbdb854fdf151cbe827123c
Rodrigo Portela <rodrigo.portela@gmail.com>
2022-12-04 23:16:55 -0300

    (in development)

--- src/py_financial/backtest/Position.py
+++ src/py_financial/backtest/Position.py
@@ -1,11 +1,16 @@
 from datetime import datetime
+from typing import Union
 
 
 class Position:
     def __init__(
-        self, ticker: str, open_time: datetime, open_price: float, short: bool = False
+        self,
+        ticker: str,
+        open_price: float,
+        open_time: Union[datetime, None] = None,
+        short: bool = False,
     ) -> None:
         self.ticker = ticker
-        self.open_time = open_time
         self.open_price = open_price
+        self.open_time = open_time if open_time else datetime.now()
         self.short = short
