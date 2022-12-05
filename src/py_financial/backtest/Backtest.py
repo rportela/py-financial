2b71fd2deff2ab5d3cbf6c08829984c41fe3e415
Rodrigo Portela <rodrigo.portela@gmail.com>
2022-12-04 23:42:40 -0300

    (in development, be patient)

--- src/py_financial/backtest/Backtest.py
+++ src/py_financial/backtest/Backtest.py
@@ -1,9 +1,71 @@
-from typing import List
+from datetime import datetime
+from typing import List, Union, Dict
 from .Position import Position
+import pandas as pd
+
 
 class Backtest:
-
     def __init__(self, capital: float) -> None:
         self.capital = capital
-        self.positions: List[Position] = []
-    
+        self.positions: Dict[str, List[Position]]
+
+    def total_profit(self) -> float:
+        profit = 0.0
+        for pos in self.positions.values():
+            for p in pos:
+                if not p.is_open:
+                    profit += p.profit
+        return profit
+
+    def total_profit_pct(self) -> float:
+        profit = 1.0
+        for pos in self.positions.values():
+            for p in pos:
+                if not p.is_open:
+                    profit *= 1 + p.profit_pct
+        return profit - 1.0
+
+    def get_last(self, ticker: str) -> Union[Position, None]:
+        vlist = self.positions.get(ticker)
+        return vlist[-1] if vlist else None
+
+    def is_last_open(self, ticker: str) -> bool:
+        last = self.get_last(ticker)
+        return last.is_open if last else False
+
+    def open(self, ticker: str, price: float, short: bool = False) -> Position:
+        last = self.get_last(ticker)
+        if last and last.short == short:
+            return last
+        else:
+            if last and last.is_open:
+                last.close(price)
+            last = Position(ticker, price, datetime.now(), short)
+            plist = self.positions.get(ticker)
+            if not plist:
+                plist = []
+                self.positions[ticker] = plist
+            plist.append(last)
+            return last
+
+    def close(self, ticker: str, price: float) -> Union[Position, None]:
+        last = self.get_last(ticker)
+        if last:
+            last.close(price)
+        return last
+
+    def positions_df(self) -> pd.DataFrame:
+        lst = []
+        for pos in self.positions.values():
+            lst += pos
+        return pd.DataFrame(lst)
+
+    def act(
+        self, ticker: str, price: float, signal: float, tolerance: float
+    ) -> Union[Position, None]:
+        if signal >= tolerance:
+            return self.open(ticker, price, True)
+        elif signal <= -tolerance:
+            return self.open(ticker, price, False)
+        else:
+            return None
--- src/py_financial/backtest/Position.py
+++ src/py_financial/backtest/Position.py
@@ -9,8 +9,20 @@ class Position:
         open_price: float,
         open_time: Union[datetime, None] = None,
         short: bool = False,
+        is_open: bool = True,
     ) -> None:
         self.ticker = ticker
         self.open_price = open_price
         self.open_time = open_time if open_time else datetime.now()
         self.short = short
+        self.is_open = is_open
+
+    def close(self, close_price: float):
+        self.is_open = False
+        self.close_price = close_price
+        self.profit = (
+            self.close_price - self.open_price
+            if not self.short
+            else self.open_price - self.close_price
+        )
+        self.profit_pct = self.profit / self.open_price
